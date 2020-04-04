24040035b00caa94c57844edd3c60b32966f395a
kimminkuk <rlaal567@gmail.com>
2020-04-05 00:54:46 +0900

    초기 커밋.

---
+++ AudioProject_0404_version2/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace AudioProject_0404_version2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(619, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "FFT Start\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(619, 406);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Clear\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart1
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(35, 23);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chart1.Series.Add(series7);
+            this.chart1.Size = new System.Drawing.Size(753, 171);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea8.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.chart2.Legends.Add(legend8);
+            this.chart2.Location = new System.Drawing.Point(35, 201);
+            this.chart2.Name = "chart2";
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series8.Legend = "Legend1";
+            series8.Name = "Series1";
+            this.chart2.Series.Add(series8);
+            this.chart2.Size = new System.Drawing.Size(753, 160);
+            this.chart2.TabIndex = 3;
+            this.chart2.Text = "chart2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 377);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(86, 21);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(486, 367);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(117, 71);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "RAW DATA";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AudioProject_0404_version2/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/****AUDIO START*****/
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+using System.Numerics;
+/****AUDIO END*****/
+
+/****THREAD START*****/
+using System.Threading;
+/****THREAD END*****/
+
+
+namespace AudioProject_0404_version2
+{
+    public partial class Form1 : Form
+    {
+        //Initial 
+        const int RATE = 44100; // Sample rate of the sound card
+        private int BUFFERSIZE = (int)Math.Pow(2, 11);
+        private Thread Real_graph;
+        public BufferedWaveProvider bwp;
+        public Int32 envelopeMax;
+        bool fft_onoff = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Initial NAudio Wave
+            int devcount = WaveIn.DeviceCount;
+            WaveIn WI = new WaveIn();
+            WI.DeviceNumber = 0;
+            WI.WaveFormat = new NAudio.Wave.WaveFormat(RATE,1); //RATE,1
+            WI.BufferMilliseconds = (int)((double)BUFFERSIZE / (double)RATE * 1000.0);
+
+            //create a wave buffer and start the recording
+            WI.DataAvailable += new EventHandler<WaveInEventArgs>(WI_DataAvailable);
+            bwp = new BufferedWaveProvider(WI.WaveFormat);
+
+            bwp.DiscardOnBufferOverflow = true;
+            WI.StartRecording();
+        }
+
+        //adds data to the audio recording buffer
+        void WI_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+
+        public void updateAudioGraph()
+        {
+            //read the bytes from the stream
+            int frameSize = BUFFERSIZE;
+            int SAMPLE_RESOLUTION = 16;
+            int BYTES_PER_POINT = SAMPLE_RESOLUTION / 8;
+
+            while (true)
+            {
+                var frames = new byte[frameSize];
+                bwp.Read(frames, 0, frameSize);
+                if (frames.Length == 0) 
+                {
+                    //textBox1.Text = "frames.Length==0 err\n"; 
+                    return;
+                }
+                if (frames[frameSize-2] == 0)
+                {
+                    //textBox1.Text = "frames[frameSize-2]==0 err\n";
+                    return;
+                }
+
+                //convert it to int32 ??
+                Int32[] vals = new int[frames.Length / BYTES_PER_POINT];
+                double[] Xs = new double[frames.Length / BYTES_PER_POINT];
+                double[] Ys = new double[frames.Length / BYTES_PER_POINT];
+                double[] Xs2 = new double[frames.Length / BYTES_PER_POINT];
+                double[] Ys2 = new double[frames.Length / BYTES_PER_POINT];
+
+                for (int i = 0; i < vals.Length; i++)
+                {
+                    //bit shift the byte buffer into the right variable format
+                    byte hByte = frames[i * 2 + 1];
+                    byte lByte = frames[i * 2 + 0];
+                    vals[i] = (int)(short)((hByte << 8) | lByte); //why?
+                    Xs[i] = i;
+                    Ys[i] = vals[i];
+                    Xs2[i] = (double)i/Ys.Length*RATE/1000.0;
+
+                }
+                if (chart1.IsHandleCreated)
+                {
+                    this.Invoke(
+                        (MethodInvoker)delegate { 
+                            update_Realtime_Graph(ref Xs, ref Ys); 
+                         }
+                    );
+                }
+                else
+                {
+                    //...
+                    //textBox1.Text += "char1.IsHandleCreated else case\n";
+                }
+
+                if (fft_onoff == true)
+                {
+                    Ys2 = FFT(Ys);
+                    if (chart2.IsHandleCreated)
+                    {
+                        this.Invoke(
+                            (MethodInvoker)delegate
+                            {
+                                update_Realtime_fft_Graph(ref Xs2, ref Ys2);
+                            }
+                        );
+                    }
+                    else
+                    {
+                        //....
+                    }
+                }
+                Thread.Sleep(10);
+            }
+        }
+
+        private void update_Realtime_Graph(ref double[] x, ref double[] y)
+        {
+            //update the displays
+            chart1.Series["Series1"].Points.Clear();
+            for(int i = 0; i < x.Length-1; i++)
+            {
+                chart1.Series["Series1"].Points.AddY(y[i]);
+            }
+        }
+
+        private void update_Realtime_fft_Graph(ref double[] x, ref double[] y)
+        {
+            //update the fft displays
+            chart2.Series["Series1"].Points.Clear();
+            for(int i = 0; i <x.Length/50-1; i++)
+            {
+                chart2.Series["Series1"].Points.AddY(y[i]);
+            }
+        }
+
+        //FFT START
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fft_onoff = true;
+            Real_graph = new Thread(new ThreadStart(this.updateAudioGraph));
+            Real_graph.IsBackground = true;
+            Real_graph.Start();
+        }
+
+        //END CODE
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fft_onoff = false;
+            Real_graph.Abort();
+            Real_graph.Join();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        
+        //NORMAL DATA
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Real_graph = new Thread(new ThreadStart(this.updateAudioGraph));
+            Real_graph.IsBackground = true;
+            Real_graph.Start();
+        }
+
+        //FFT MATH
+        public double[] FFT(double[] data)
+        {
+            double[] fft = new double[data.Length]; //this is where is where we will store the output(fft)
+            Complex[] fftComplex = new Complex[data.Length]; // the FFT function requires complex format
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                fftComplex[i] = new Complex(data[i], 0.0);
+            }
+
+            Accord.Math.FourierTransform.FFT(fftComplex, Accord.Math.FourierTransform.Direction.Forward);
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                fft[i] = fftComplex[i].Magnitude; //back to double
+                //fft[i] = Math.Log10(fft[i]); //convert to DB
+            }
+            return fft;
+        }
+    }
+}
